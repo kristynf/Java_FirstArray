ddcb58ea5176b53471f0cb6a4bee3fadfd1553d9
kristynf <thetruthandsoul@gmail.com>
2020-01-28 08:34:38 -0500

    added matching

--- src/ArrayReplace.java
+++ src/ArrayReplace.java
@@ -1,19 +1,31 @@
-import java.util.ArrayList;
+/*import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class ArrayReplace {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         ArrayList<String> arr1 = new ArrayList<>();
         arr1.add("w");
         arr1.add("t");
         arr1.add("y");
         arr1.add("h");
         arr1.add("k");
-        for (int i =0; i < arr1.size(); i++){
-            if (arr1[i]= ){
 
 
+        ListIterator<String> iterator = arr1.listIterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            if (next.equals("t")) {
+                //Replace element
+                iterator.set("hello");
+
+                for (String name:arr1){
+
+                    System.out.println(name);}*/
+
+
+            }
+
+
+        }
     }
-
-
-
-
 }
---
+++ src/Matching.java
@@ -0,0 +1,26 @@
+import java.util.stream.BaseStream
+
+i
+public class Matching {
+    public static void main(String [] arg){
+        int [] array1 = new int[]{1,7,6,5,9};
+        int [] array2 = new int[]{2,7,6,3,4};
+
+        public static <T> Stream<T> stream(T[] array)
+        // If any number from List is present in List 2
+        System.out.println(
+                "If any number from aList is present in List 2 :")
+                        array1.stream().anyMatch(int - > array2.contains(num));
+
+
+
+
+
+}
+
+
+
+//9. Given array 1: [1,7,6,5,9] and array 2: [2,7,6,3,4]
+//        Write a program that will print out all matching pairs from arrays 1 and 2.
+//        Expected output:
+//        (7,7) and (6,6)
